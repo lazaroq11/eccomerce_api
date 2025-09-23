978c71219b94cdacb6fc014fe3056a00742d1da8
lazaroq11 <lazaroq11@hotmail.com>
2025-09-23 14:52:24 -0300

    ajustando

---
+++ api/index.ts
@@ -0,0 +1,18 @@
+import { createServer, proxy } from 'aws-serverless-express';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express'; // <- CORREÇÃO
+
+const expressApp = express();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  app.enableCors();
+  await app.init();
+}
+
+bootstrap();
+
+const server = createServer(expressApp);
+export const handler = (req, res) => proxy(server, req, res);
