3d0079f2a9cdddd8a2e5cc206a08f4792890f86d
lazaroq11 <lazaroq11@hotmail.com>
2025-09-23 16:31:26 -0300

    feat: add prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,20 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
 
+declare global {
+    var prisma: PrismaClient | undefined;
+}
+
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit  { 
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
+        super();
+        if (!global.prisma) {
+            global.prisma = this;
+        }
+
+    }
+
     async onModuleInit() {
         await this.$connect();
     }
